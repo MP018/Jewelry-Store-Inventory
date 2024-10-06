845f7bfcac59460c9adc88ad43919e954c334cf0
frop <pcu1298@gmail.com>
2024-10-06 15:22:41 -0400

    update palace_jewelers.sql

--- palace_jewelers.sql
+++ palace_jewelers.sql
@@ -1,17 +1,70 @@
 use palace_jewelers;
-
 show tables;
 
-describe item;
+create table Item (
+SKU varchar(500) not null primary key,
+pictures varchar(500) not null,
+description varchar(500) not null,
+price decimal(7,2) not null,
+name varchar(500) not null,
+quantity int not null,
+sold_date date
+);
 
-CREATE TABLE `item` (
-    `pictures` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-    `description` varchar(500) DEFAULT NULL,
-    `price` decimal(7, 2) DEFAULT NULL,
-    `name` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-    `type` varchar(255) DEFAULT NULL,
-    `metal` varchar(255) DEFAULT NULL,
-    `quantity` int DEFAULT NULL,
-    `soldDate` date DEFAULT NULL,
-    `seller` varchar(255) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+create table Item_Tags (
+sku varchar(500) not null primary key,
+tags varchar(500) not null,
+constraint fk_sku foreign key (sku) references item (sku)
+);
+
+create table Customer (
+Customer_ID int primary key not null,
+First_Name varchar(500) not null,
+Last_Name varchar(500) not null,
+phone int not null,
+email varchar(500) not null
+);
+
+create table Employee (
+Employee_ID int not null primary key,
+First_Name varchar(500) not null,
+Last_Name varchar(500) not null,
+Position varchar(500) not null,
+Email varchar(500) not null,
+phone int not null
+);
+
+create table Repair_Order (
+Repair_ID int primary key not null,
+Employee_ID int not null,
+Customer_ID int not null,
+Subtotal decimal(7,2) not null,
+Tax decimal(7,2) not null,
+Total decimal(7,2) not null,
+constraint fk_repair_Id foreign key (repair_id) references repair_item (repair_id)
+);
+
+create table Repair_Item (
+Repair_ID int not null primary key,
+customer_id int not null,
+pictures varchar(500) not null,
+description varchar(500) not null,
+name varchar(500) not null
+);
+
+create table Order_Item (
+Order_Number int primary key not null,
+SKU varchar(500) not null
+);
+
+create table Sale (
+Order_Number int primary key not null,
+Customer_ID int not null,
+Employee_ID int not null,
+Subtotal decimal(7,2) not null,
+Tax decimal(7,2) not null,
+Total decimal(7,2) not null,
+constraint fk_customerid foreign key (Customer_ID) references Customer (customer_id),
+constraint fk_employeeid foreign key (employee_id) references Employee (employee_id),
+constraint fk_order_number foreign key (Order_Number) references order_item (order_number)
+);
