1b8f720f0b6e8c1d995cce95c87383215fbb416b
frop <pcu1298@gmail.com>
2024-10-16 13:37:27 -0400

    Connected registration.html to database
    front end has to create page for login.html to go to

--- Website/app.py
+++ Website/app.py
@@ -12,7 +12,7 @@ app.secret_key = ':)'
 app.config['MYSQL_HOST'] = 'palacejewelers-palacejewelers.j.aivencloud.com'
 app.config['MYSQL_USER'] = 'avnadmin'
 app.config['MYSQL_PASSWORD'] = 'AVNS_eTE1cr2Go3sTM_VZneL'
-app.config['MYSQL_DB'] = 'defaultdb'
+app.config['MYSQL_DB'] = 'PalaceDatabase'
 app.config['MYSQL_PORT'] = 16246
 
 # Intialize MySQL
@@ -22,42 +22,35 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
     msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        email = request.form['email']
-        password = request.form['password']
-        # Check if account exists using MySQL
+    if request.method == 'POST' and 'Customer_Email' in request.form and 'Customer_Password' in request.form:
+        email = request.form['Customer_Email']
+        password = request.form['Customer_Password']
+        
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM Customer WHERE email = %s AND password = %s', (email, password,))
-        # Fetch one record and return result
+        cursor.execute('SELECT * FROM CUSTOMER WHERE Customer_Email = %s AND Customer_Password = %s', (email, password,))
         account = cursor.fetchone()
-        # If account exists in the database
+
         if account:
-            # Create session data, we can access this data in other routes
             session['loggedin'] = True
-            session['id'] = account['id']
-            session['First_Name'] = account['firstName']
-            session['Last_Name'] = account['lastName']
-            session['email'] = account['email']
-            session['password'] = account['password']
-            # Redirect to home page or other page
-            return redirect(url_for('home'))  # Redirect after successful login
+            session['Customer_ID'] = account['Customer_ID']
+            session['First_Name'] = account['First_Name']
+            session['Last_Name'] = account['Last_Name']
+            session['Customer_Email'] = account['Customer_Email']
+            return redirect(url_for('home'))
         else:
-            # Account doesn't exist or incorrect username/password
-            msg = 'Incorrect username/password!'
-    # Show the login form with a message (if any)
+            msg = 'Incorrect email/password!'
+
     return render_template('login.html', msg=msg)
+
 
 
 @app.route('/logout')
 def logout():
     # Remove session data, this will log the user out
    session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('email', None)
+   session.pop('Customer_ID', None)
+   session.pop('Customer_Email', None)
    # Redirect to login page
    return redirect(url_for('login'))
 
@@ -66,42 +59,40 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
-        name = request.form['name']
-        firstName, lastName = name.split(" ", 1)
-        password = request.form['password']
-        email = request.form['email']
+    if request.method == 'POST':
+        print("Form submitted!")  # Debugging statement
+        if 'name' in request.form and 'Customer_Password' in request.form and 'Customer_Email' in request.form:
+            name = request.form['name']
+            firstName, lastName = name.split(" ", 1)
+            password = request.form['Customer_Password']
+            email = request.form['Customer_Email']
 
-        # Debugging output
-        print(f"Name: {name}, First Name: {firstName}, Last Name: {lastName}, Email: {email}, Password: {password}")
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM CUSTOMER WHERE Customer_Email = %s', (email,))
+            account = cursor.fetchone()
 
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM Customer WHERE email = %s', (email,))
-        account = cursor.fetchone()
-
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', name):
-            msg = 'Username must contain only characters and numbers!'
-        elif not name or not password or not email:
-            msg = 'Please fill out the form!'
-        else:
-            try:
-                # Attempt to insert into the database
-                cursor.execute('INSERT INTO Customer (First_Name, Last_Name, password, email) VALUES (%s, %s, %s, %s)', (firstName, lastName, password, email,))
-                mysql.connection.commit()
-                msg = 'You have successfully registered!'
-                print("Registration successful!")
-            except MySQLdb.Error as err:
-                print(f"Database error: {err}")
-                msg = 'There was an issue with registration. Please try again.'
-    
-    elif request.method == 'POST':
-        msg = 'Please fill out the form!'
+            if account:
+                msg = 'Account already exists!'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                msg = 'Invalid email address!'
+            elif not re.match(r'[A-Za-z0-9 ]+', name):
+                msg = 'Name must contain only characters and numbers!'
+            elif not name or not password or not email:
+                msg = 'Please fill out the form!'
+            else:
+                try:
+                    cursor.execute('INSERT INTO CUSTOMER (First_Name, Last_Name, Customer_Phone, Customer_Email, Customer_Password) VALUES (%s, %s, %s, %s, %s)', 
+                                   (firstName, lastName, None, email, password,))
+                    mysql.connection.commit()
+                    msg = 'You have successfully registered!'
+                    print("Registration successful!")
+                except MySQLdb.Error as err:
+                    print(f"Database error: {err}")
+                    msg = 'There was an issue with registration. Please try again.'
 
     return render_template('registration.html', msg=msg)
+
+
 
 
 if __name__ == '__main__':
