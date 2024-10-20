8fdbd12f93312fedbdf510c5fa55dd1a0d2b4596
Reece Cohen <145138366+ReeceC747@users.noreply.github.com>
2024-10-19 23:35:31 -0400

    Update app.py
    App now functions with dashboard and customer profile services

--- Website/app.py
+++ Website/app.py
@@ -19,41 +19,60 @@ app.config['MYSQL_PORT'] = 16246
 mysql = MySQL(app)
 
 # http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
-
 @app.route('/', methods=['GET', 'POST'])
 def login():
     msg = ''
-    if request.method == 'POST' and 'Customer_Email' in request.form and 'Customer_Password' in request.form:
-        email = request.form['Customer_Email']
-        password = request.form['Customer_Password']
+    if request.method == 'POST' and 'Employee_Email' in request.form and 'Employee_Password' in request.form:
+        email = request.form['Employee_Email']
+        password = request.form['Employee_Password']
         
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM CUSTOMER WHERE Customer_Email = %s AND Customer_Password = %s', (email, password,))
+        cursor.execute('SELECT * FROM EMPLOYEE WHERE Employee_Email = %s AND Employee_Password = %s', (email, password,))
         account = cursor.fetchone()
 
         if account:
             session['loggedin'] = True
-            session['Customer_ID'] = account['Customer_ID']
+            session['Employee_ID'] = account['Employee_ID']
             session['First_Name'] = account['First_Name']
             session['Last_Name'] = account['Last_Name']
-            session['Customer_Email'] = account['Customer_Email']
-            return redirect(url_for('home'))
+            session['Employee_Email'] = account['Employee_Email']
+            return redirect(url_for('dashboard'))
         else:
             msg = 'Incorrect email/password!'
 
     return render_template('login.html', msg=msg)
 
+# Dashboard page
+@app.route('/dashboard')
+def dashboard():
+    # Ensure user is logged in before showing the dashboard
+    if 'loggedin' in session:
+        return render_template('dashboard.html', 
+                               first_name=session['First_Name'], 
+                               last_name=session['Last_Name'])
+    # If not logged in, redirect to login page
+    return redirect(url_for('login'))
 
+@app.route('/customer_profile')
+def customer_profile():
+    # Ensure user is logged in before showing the customer profile
+    if 'loggedin' in session:
+        return render_template('customer_profile.html', 
+                               first_name=session['First_Name'], 
+                               last_name=session['Last_Name'])
+    # If not logged in, redirect to login page
+    return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
-    # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('Customer_ID', None)
-   session.pop('Customer_Email', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
-
+    # Remove user session data
+    session.pop('loggedin', None)
+    session.pop('Employee_ID', None)
+    session.pop('Employee_Email', None)
+    session.pop('First_Name', None)
+    session.pop('Last_Name', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
 
 # http://localhost:5000/Falsk/register - this will be the registration page, we need to use both GET and POST requests
 @app.route('/register', methods=['GET', 'POST'])
@@ -61,14 +80,14 @@ def register():
     msg = ''
     if request.method == 'POST':
         print("Form submitted!")  # Debugging statement
-        if 'name' in request.form and 'Customer_Password' in request.form and 'Customer_Email' in request.form:
+        if 'name' in request.form and 'Employee_Password' in request.form and 'Employee_Email' in request.form:
             name = request.form['name']
             firstName, lastName = name.split(" ", 1)
-            password = request.form['Customer_Password']
-            email = request.form['Customer_Email']
+            password = request.form['Employee_Password']
+            email = request.form['Employee_Email']
 
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute('SELECT * FROM CUSTOMER WHERE Customer_Email = %s', (email,))
+            cursor.execute('SELECT * FROM EMPLOYEE WHERE Employee_Email = %s', (email,))
             account = cursor.fetchone()
 
             if account:
@@ -81,17 +100,17 @@ def register():
                 msg = 'Please fill out the form!'
             else:
                 try:
-                    cursor.execute('INSERT INTO CUSTOMER (First_Name, Last_Name, Customer_Phone, Customer_Email, Customer_Password) VALUES (%s, %s, %s, %s, %s)', 
-                                   (firstName, lastName, None, email, password,))
+                    #add password for employee
+                    cursor.execute('INSERT INTO EMPLOYEE (First_Name, Last_Name, Employee_Email, Employee_Password) VALUES (%s, %s, %s, %s)', 
+                                   (firstName, lastName, email, password))
                     mysql.connection.commit()
                     msg = 'You have successfully registered!'
                     print("Registration successful!")
+                    return redirect(url_for('login'))
                 except MySQLdb.Error as err:
                     print(f"Database error: {err}")
                     msg = 'There was an issue with registration. Please try again.'
-
     return render_template('registration.html', msg=msg)
-
 
 
 
